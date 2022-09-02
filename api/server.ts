ac8f3710d512c9c89207402a611a7434cc234c4a
Luciano Lim <lmlim@cpp.edu>
2022-09-02 14:19:31 -0700

    init

---
+++ api/server.ts
@@ -0,0 +1,12 @@
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post('/', (req, res) => {
+  res.send(req.body.test || 'Hello World');
+});
+
+app.listen(process.env.PORT ?? 3000);
