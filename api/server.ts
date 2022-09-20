1f0109ba959aba6b753b85228cc79a8226d29df8
peppacaiou <109382836+peppacaiou@users.noreply.github.com>
2022-09-20 00:00:48 -0700

    Implemented basic professor and search endpoints

--- api/server.ts
+++ api/server.ts
@@ -6,7 +6,21 @@ app.use(express.json());
 app.use(cors());
 
 app.post('/', (req, res) => {
+  // algorithm for random list
+  let teachers = ["here", "are","some","shit"]
+  //algorithm end 
+  
+  res.send(req.body.test || 'Hello World');
+});
+
+app.post('/', (req, res) => {
+  // algorithm for random list
+  let teachers = ["here", "are","some","shit"]
+  //algorithm end 
+  
   res.send(req.body.test || 'Hello World');
 });
 
 app.listen(process.env.PORT ?? 3000);
+
+//https://reqbin.com/
