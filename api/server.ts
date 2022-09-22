13be478a64e7fb7738da0ac1632d021d1be1651d
peppacaiou <109382836+peppacaiou@users.noreply.github.com>
2022-09-21 17:24:36 -0700

    implemented boilerplate code and should be ready for merge

--- api/server.ts
+++ api/server.ts
@@ -7,15 +7,28 @@ app.use(cors());
 
 app.post('/professor', (req, res) => {
   // API returns single professor data or null if doesn't exist
+  //I'm using a single object, but Ideally this should be a nested dictionary? this is the format that I'll just stick with 
+  let professorReturn = {
+    "BroncoDirect Name": "Name",
+    "Name" : "Name",
+    "RMP" : "Name",
+    "RMP Name": "Name",
+    "Difficulty": 1,  //1-10
+    "TakeAgain": 4.2  //1.0- 5.0
+
+  }
+  // BroncoDirect Name, RMP Name, RMP URL, Rating, Difficulty, TakeAgain(float)
   
-  
-  res.send(req.body.test || 'in prof');
+  return res.send(professorReturn|| 'in prof');
 });
 app.post('/search', (req, res) => {
   // returns random list of professors
+  let searchReturn ={
+    profs :[1,2,3,4,5,6,7,8,9] //remember this should return actual professor names 
+  }
 
   
-  res.send(req.body.test || 'in search');
+  res.send(searchReturn || 'contact peppacaiou');
 });
 
 
