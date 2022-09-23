e15c92198f9befca5362236b915903d4a418b385
peppacaiou <109382836+peppacaiou@users.noreply.github.com>
2022-09-22 17:38:44 -0700

    updated regarding eslint guidlines

--- api/server.ts
+++ api/server.ts
@@ -7,14 +7,14 @@ app.use(cors());
 
 app.post('/professor', (req, res) => {
   // API returns single professor data or null if doesn't exist
-  //I'm using a single object, but Ideally this should be a nested dictionary? this is the format that I'll just stick with 
-  let professorReturn = {
+  // I'm using a single object, but Ideally this should be a nested dictionary? this is the format that I'll just stick with 
+  const professorReturn = {
     "BroncoDirect Name": "Name",
     "Name" : "Name",
     "RMP" : "Name",
     "RMP Name": "Name",
-    "Difficulty": 1,  //1-10
-    "TakeAgain": 4.2  //1.0- 5.0
+    "Difficulty": 1,  // 1-10
+    "TakeAgain": 4.2  // 1.0- 5.0
 
   }
   // BroncoDirect Name, RMP Name, RMP URL, Rating, Difficulty, TakeAgain(float)
@@ -23,8 +23,8 @@ app.post('/professor', (req, res) => {
 });
 app.post('/search', (req, res) => {
   // returns random list of professors
-  let searchReturn ={
-    profs :[1,2,3,4,5,6,7,8,9] //remember this should return actual professor names 
+  const searchReturn ={
+    profs :[1,2,3,4,5,6,7,8,9] // remember this should return actual professor names 
   }
 
   
@@ -34,4 +34,4 @@ app.post('/search', (req, res) => {
 
 app.listen(process.env.PORT ?? 3000);
 
-//https://reqbin.com/
+// https://reqbin.com/
