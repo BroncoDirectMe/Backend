1edcffbd0e939f2dd998ce60597b5120c05b0fc7
E-A-H62 <eahernandez1212@yahoo.com>
2022-10-19 12:06:30 -0700

    Wrote tests

--- test/server.test.ts
+++ test/server.test.ts
@@ -1,8 +1,27 @@
-import { expect } from "chai";
+import express from 'express';
+import {expect} from "chai";
+const chai = require('chai');
+const app = express();
 
 describe("basic test", function() {
     it("should return true", function() {
-      expect(true).to.equal(false);
+        expect(true).to.equal(true);
     });
-    
+})
+
+//testing the professors hard coded into the mapping
+describe("check professor data", function() {
+    it("should get professor data", function(done) {
+        let data = app.post('Hao Ji');
+        expect(data).to.have.property('Hao Ji');
+        done();
+    });
+})
+
+describe("check professor data", function() {
+    it("should get professor data", function(done) {
+        let data = app.post('Ben Steichen');
+        expect(data).to.have.property('Ben Steichen');
+        done();
+    });
 })
