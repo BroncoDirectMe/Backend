ded1697f2815c548dc6c62efc225237cfa929a49
peppacaiou <109382836+peppacaiou@users.noreply.github.com>
2022-11-04 00:17:28 -0700

    fixed some formatting

--- test/server.test.ts
+++ test/server.test.ts
@@ -41,7 +41,7 @@ describe('[Professor] 3 test cases:', function () {
       'rmpName',
       'rmp',
       'difficulty',
-      'takeAgain',
+      'takeClassAgain',
     ];
     expect(res.body).to.be.an('object').to.have.all.keys(keys);
     keys.forEach((element) => {
@@ -76,18 +76,16 @@ describe('[Search] 3 test cases', function () {
         .that.includes({ err: 'please specify a number' });
   });
   it('correct ', async function () {
-    const nameSend = {
-      name: 'val',
-    };
-    const res = await request(server).post('/search').send(nameSend);
-    const keys = ['array'];
-    expect(res.body).to.be.an('object').to.have.all.keys(keys);
-    keys.forEach((element) => {
-      if (element === 'number') {
-        expect(res.body[element]).to.be.an('number');
-      } else {
-        expect(res.body[element]).to.be.a('string');
-      }
-    });
+        const nameSend = {      
+            count: 1
+            };    
+        const res = await request(server).post('/search').send(nameSend);    
+        console.log(res.body)
+        // const keys = ['profs'];    
+        // expect(res.body).to.be.an('object').to.have.all.keys(res.body.profs);
+        // expect(res.body.profs).to.be.a('array');
+        res.body.profs.forEach((element: String) => {      
+            expect(element).to.be.a('string');    
+        });  
   });
 });
