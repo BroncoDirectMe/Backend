835af62c8b2d13fad70bc6c12bd5ac4564773a4a
E-A-H62 <85127520+E-A-H62@users.noreply.github.com>
2022-11-23 22:38:35 -0800

    Leaf/peppacaiou/making endpoints for upvoting and downvoting (#35)
    * DOES THIS SHIT FUCKING WORK
    
    * Wrote tests
    
    * Implemented basic professor and search endpoints
    
    * implemented basic testing procedures for professors
    
    * updated server to return objects instead of strings on errors
    
    * formatted files with prettier. Also finalized professor tests
    
    * Formatted with eslint this time
    
    * formatted test file so eslint should stfu now
    
    * Wrote tests cases for /search
    
    * quick commit before pull
    
    * Fixed test case in [Search]
    
    * Formatted with Prettier
    
    * added boilerplate code
    
    * fixed some formatting
    
    * fixed some minor formatting issues
    
    Co-authored-by: peppacaiou <109382836+peppacaiou@users.noreply.github.com>

--- api/server.ts
+++ api/server.ts
@@ -9,8 +9,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-function checkEmpty(values: object): boolean {
-  return Object.keys(values).length === 0;
+function checkEmpty(content: object, res: any): boolean {
+  if (Object.keys(content).length === 0) {
+    res.status(400).send({ err: 'empty json not accepted' });
+    return true;
+  }
+  return false;
 }
 
 /* eslint-disable @typescript-eslint/naming-convention */
@@ -25,13 +29,13 @@ app.post('/professor', async (req, res) => {
   // I'm using a single object, but Ideally this should be a nested object? this is the format that I'll just stick with
   // object input
   // {"name" : "professor"}
-  if (checkEmpty(req.body)) {
-    res.status(400).send('empty json not accepted');
+  if (checkEmpty(req.body, res)) {
     return;
   }
   if (!('name' in req.body)) {
-    res.status(400).send('name of professor needs to be specified');
-    return;
+    return res
+      .status(400)
+      .send({ err: 'name of professor needs to be specified' });
   }
 
   const name: string = req.body.name;
@@ -42,8 +46,7 @@ app.post('/professor', async (req, res) => {
     data = await getProfessorByName(name);
   } else {
     if (!(name in tempMapping)) {
-      res.status(400).send('professor not found in mapping');
-      return;
+      return res.status(400).send('professor not found in mapping');
     }
     data = await getProfessorByName(tempMapping[name]);
   }
@@ -58,22 +61,68 @@ app.post('/professor', async (req, res) => {
 
 app.post('/search', async (req, res) => {
   // returns random list of professors
-  if (checkEmpty(req.body)) {
-    res.status(400).send('please provide a json with key of count');
+  if (checkEmpty(req.body, res)) {
     return;
   }
   if ('count' in req.body && !Number.isInteger(req.body.count)) {
-    res.status(400).send('count parameter must be number');
-    return;
+    return res.status(400).send('count parameter must be number');
   }
   if (!('name' in req.body)) {
-    res.status(400).send('name of professor needs to be specified');
-    return;
+    return res.status(400).send('name of professor needs to be specified');
   }
 
   const data = await getProfessorSearch(req.body.name);
 
   res.send(data);
+});
+
+app.post('/upvote', async (req, res) => {
+  const SEND_DATA: { [key: string]: number } = {};
+  if (checkEmpty(req.body, res)) {
+    return;
+  }
+  if (!('professor' in req.body)) {
+    return res.status(400).send('Professor key is missing');
+  }
+  try {
+    req.body.professor.forEach((item: string) => {
+      SEND_DATA[item] = 1;
+    });
+    res.send(SEND_DATA);
+  } catch (error: unknown) {
+    const ERROR_MESSAGE = error;
+    res.send(ERROR_MESSAGE);
+  }
+});
+
+app.post('/downvote', async (req, res) => {
+  const SEND_DATA: { [key: string]: number } = {};
+  if (checkEmpty(req.body, res)) {
+    return;
+  }
+  if (!('professor' in req.body)) {
+    return res.status(400).send('Professor key is missing');
+  }
+  try {
+    req.body.professor.forEach((item: string) => {
+      SEND_DATA[item] = 0;
+    });
+    res.send(SEND_DATA);
+  } catch (error: unknown) {
+    const ERROR_MESSAGE = error;
+    res.send(ERROR_MESSAGE);
+  }
+});
+
+app.get('/vote', (req, res) => {
+  if (checkEmpty(req.body, res)) {
+    return;
+  }
+
+  const result = {
+    profName: 40,
+  };
+  return res.status(400).send(result);
 });
 
 app.listen(process.env.PORT ?? 3000);
