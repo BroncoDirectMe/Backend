d4781d7a1d1e3e89d7b8fe1e5d6810473effa903
OJ101003 <73129639+OJ101003@users.noreply.github.com>
2024-03-08 15:07:59 -0800

    Removed comments curriculum.py

--- scraper/curriculum/curriculum/spiders/curriculum.py
+++ scraper/curriculum/curriculum/spiders/curriculum.py
@@ -1,45 +1,26 @@
-# Ignore the bad variable names, just focus on what the comments explain about the scraper
-
-# How to use
-"""
-Make sure you have scrapy installed
-1. Change start url to the Index of Academic Programs page of the year you want and save the file. the one included is for the 2023-2024 year
-2. Open a terminal in the folder where curriculum.py is in
-3. In the terminal put scrapy crawl curriculum -O "put file name here".csv
-4. Let it run till it finishes, there should be a csv file in the same folder.
-Now repeat that for the other 4 years, should just be as easy as replacing the starting url. 
-"""
-
-# Comments here are for myself and others to understand what's going on here.
-
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 import re
 class CurriculumScraper(CrawlSpider):
     name = "curriculum"
-    start_urls = ["https://catalog.cpp.edu/content.php?catoid=65&navoid=5519"] # This is the start url, change this manually to the index of Academic Programs page of the year you are interested in scraping
+    start_urls = ["https://catalog.cpp.edu/content.php?catoid=65&navoid=5519"] 
 
     rules = [
         Rule( 
-            LinkExtractor(restrict_text=r'(?P<name>units)'), # This tells the scraper which links to click on, which are any links that have the word units in them
-            callback="parse_page" # Calls the parse_page function below
+            LinkExtractor(restrict_text=r'(?P<name>units)'), 
+            callback="parse_page" 
             )
         ]
     def parse_page(self, response):
-        # First part of the code is responsable for getting the programs name, required courses, and electives.
-        # Second part of the code is responsable for getting the requirements for that program
-        
-        # First Part of code
-        test = response.css('div.acalog-core') # Once again, ignore bad variable names. There's an explanation for what the variables do at the end of the first part
+        test = response.css('div.acalog-core') 
         test2 = test.css("div.acalog-core ul")
-        pattern = r'>([^<]+)<\/a>' # Regex pattern for getting context between > and </a> tags, which helps get the name of the course
-        majorCourses = test2[0].css('li.acalog-course span').getall() # Gets the required courses
+        pattern = r'>([^<]+)<\/a>' 
+        majorCourses = test2[0].css('li.acalog-course span').getall()
         electiveCourses = [] 
         for i in range(1, len(test2)):
-            electiveCourses.extend(test2[i].css('li.acalog-course span').getall()) # Gets the remaining courses that exclude the required courses, electives
+            electiveCourses.extend(test2[i].css('li.acalog-course span').getall())
 
-        # These for loops use the regex pattern to remove all the non important text from the strings, and then replaces the "dirty" text with the cleaned text
         for index,course in enumerate(majorCourses):
             name = re.search(pattern, course).group(1)
             majorCourses[index] = name
@@ -47,30 +28,21 @@ class CurriculumScraper(CrawlSpider):
         for index,course in enumerate(electiveCourses):
             name = re.search(pattern, course).group(1)
             electiveCourses[index] = name
-
-        # test = response.css('div.acalog-core') This contains all instances of acalog-core found on the page
-        # test2 = test.css("div.acalog-core ul") This gets further instances of acalog-core with ul and puts it in a list
-        # li.acalog-course span gets any li tag with the class acalog-course, and then gets instances of span within those li.acalog-courses
-        # test2[i].css('li.acalog-course span').getall() Returns the list of courses for that block as a list
-        # test2[0] should be required courses, everything else is electives
-
-        # Second Part of code
-        # Ignore list that contains all the tags that we want to ignore, contains edge cases too
+            
         ignoreList = {'Curriculum','General Education Requirements: 48 units', 'American Cultural Perspectives Requirement: 3 units','Interdisciplinary General Education: 21 units','Interdisciplinary General Education: 18 units', 'American Institutions: 6 units', 'Graduation Writing Test', 'Note(s):'}
-        allNames =  response.css("div.acalog-core h2::text").extract() # Extracts all the h2 tags on the page into text
+        allNames =  response.css("div.acalog-core h2::text").extract() 
         selectNames = []
-        # Handles edge case where sometimes the page has the h2 as curriculum and then has the actual names we need under it
         if len(allNames) == 1:
             if allNames[0] == 'Curriculum':
-                allNames = response.css("div.acalog-core h3::text").extract() # Replaces allNames with tags in edge case
+                allNames = response.css("div.acalog-core h3::text").extract() 
 
-        for i in allNames: # Goes through the list of all the names we gathered
-            name = i.replace("*","") # Removes any asteriks found, specifically for when "Note(s)" might be put as "Note(s)*" and such
-            name = name.strip() # Removes whitespaces at the ends
-            if name not in ignoreList: # Makes sure name isn't in the ignore list, then appends it to the selectNames list, which contains the names we want
+        for i in allNames:
+            name = i.replace("*","") 
+            name = name.strip() 
+            if name not in ignoreList: 
                 selectNames.append(name)
 
-        yield{ # Returns 4 things, the major name, the required courses, the electives, and the requirements
+        yield{
             "Program Name" : response.css("h1 ::text").get(),
             "Required Courses": majorCourses,
             "Electives" : electiveCourses,
