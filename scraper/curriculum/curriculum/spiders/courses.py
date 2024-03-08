78b64f92d22fc10c9299cc0c2d0cadaefd2555a7
OJ101003 <73129639+OJ101003@users.noreply.github.com>
2024-03-08 15:09:09 -0800

    Removed comments courses.py

--- scraper/curriculum/curriculum/spiders/courses.py
+++ scraper/curriculum/curriculum/spiders/courses.py
@@ -1,11 +1,3 @@
-"""
-Make sure you have scrapy installed
-1. Change start url to the Index of Academic Programs page of the year you want and save the file. the one included is for the 2023-2024 year
-2. Open a terminal in the folder where curriculum.py is in
-3. In the terminal put scrapy crawl course -O "put file name here".csv
-4. Let it run till it finishes, there should be a csv file in the same folder.
-Now repeat that for the other 4 years, should just be as easy as replacing the starting url. 
-"""
 
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
@@ -13,7 +5,7 @@ from scrapy.linkextractors import LinkExtractor
 import re
 class CourseScraper(CrawlSpider):
     name = "course"
-    start_urls = ["https://catalog.cpp.edu/content.php?catoid=65&navoid=5519"] # Change this manually to the index of Academic Programs page of the year you are interested in scraping
+    start_urls = ["https://catalog.cpp.edu/content.php?catoid=65&navoid=5519"] 
 
     rules = [
         Rule(
@@ -41,7 +33,6 @@ class CourseScraper(CrawlSpider):
         course_name = response.css('#course_preview_title::text').get(default='').strip()
 
         def extract_text(starting_phrase):
-            # Locate the <strong> element containing the starting phrase
             following_nodes = response.xpath(
                 f'//strong[contains(text(), "{starting_phrase}")]/following-sibling::node()'
             )
@@ -49,16 +40,13 @@ class CourseScraper(CrawlSpider):
             for node in following_nodes:
                 node_string = node.extract()
                 if '<br>' in node_string:
-                    break  # Stop collecting text once a <br> element is encountered
+                    break  
                 elif '<a' in node_string:
-                    # Extract text content from <a> elements
                     link_text = re.search(r'>([^<]+)<', node_string)
                     if link_text:
                         text_chunks.append(link_text.group(1).strip())
                 elif '<span' not in node_string:
-                    # Collect text from text nodes while ignoring <span> elements
                     text_chunks.append(node_string.strip())
-            # Join all collected text chunks into a single string, replacing multiple spaces with a single space
             text = ' '.join(text_chunks)
             text = re.sub(r'\s+', ' ', text)
             return text.strip()
